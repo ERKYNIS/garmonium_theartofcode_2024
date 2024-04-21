737c8946298ff15cc5e85d953a9a82fbb3db8261
Nikita Belkin <76586422+ERKYNIS@users.noreply.github.com>
2024-04-21 08:57:57 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -34,6 +34,22 @@ def databaserequest(text, params=None, commit=False):
     return True
 
 
+def isloggin():
+    if request.cookies.get("id") and request.cookies.get("email") and request.cookies.get("password"):
+        testacc = databaserequest("SELECT * FROM accounts WHERE `email`=? AND `password`=?",
+                                  params=[request.cookies.get("email"), request.cookies.get("password")])
+        if len(testacc) != 0:
+            return True
+        else:
+            resp = app.make_response(render_template("process.html", title="Ошибка получения информации об аккаунте...",
+                                                     redirect="/login"))
+            resp.set_cookie('acc_id', f'', max_age=2592000)
+            resp.set_cookie('email', f'', max_age=2592000)
+            resp.set_cookie('password', f'', max_age=2592000)
+            return resp
+    return False
+
+
 def render_page(template, title="", loggin=False, acc_access=[0], **kwargs):
     if loggin and not isloggin():
         return redirect('/login')
@@ -44,18 +60,12 @@ def render_page(template, title="", loggin=False, acc_access=[0], **kwargs):
         return render_template(template, title=title, **kwargs)
 
 
-def isloggin():
-    if request.cookies.get("id"):
-        return True
-    return False
-
-
 def check_password(password):
     digits = '1234567890'
     upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     lower_letters = 'abcdefghijklmnopqrstuvwxyz'
     symbols = '!@#$%^&*()-+'
-    acceptable = digits+upper_letters+lower_letters+symbols
+    acceptable = digits + upper_letters + lower_letters + symbols
 
     passwd = set(password)
     if any(char not in acceptable for char in passwd):
@@ -63,16 +73,16 @@ def check_password(password):
     else:
         recommendations = []
         if len(password) < 12:
-            recommendations.append(f'увеличить число символов - {12-len(password)}')
-        for what, message in ((digits,        'цифру'),
-                              (symbols,       'спецсимвол'),
+            recommendations.append(f'увеличить число символов - {12 - len(password)}')
+        for what, message in ((digits, 'цифру'),
+                              (symbols, 'спецсимвол'),
                               (upper_letters, 'заглавную букву'),
                               (lower_letters, 'строчную букву')):
             if all(char not in what for char in passwd):
                 recommendations.append(f'добавить 1 {message}')
 
         if recommendations:
-            return "Слабый пароль. Рекомендации:", ", ".join(recommendations)
+            return 'Слабый пароль.<br><br>Рекомендации:<br>' + ",<br>".join(recommendations)
         else:
             return "ok"
 
@@ -84,37 +94,110 @@ def main():
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    if isloggin():
+        return redirect('/account')
     if request.method == 'POST':
         testuser = databaserequest("SELECT * FROM accounts WHERE `email` = ?", params=[request.form.get("email")])
         if len(testuser) == 0:
             error = check_password(request.form.get("password"))
             if error == "ok":
+                if request.form.get("password") != request.form.get("password2"):
+                    return render_page('register.html', title="Регистрация", error="Пароль и его повтор не совпадают!")
+                md5password = request.form.get("password")
+                md5password = md5password.encode(encoding='UTF-8', errors='strict')
+                md5password = str(hashlib.md5(md5password).hexdigest())
                 databaserequest("INSERT INTO accounts(`email`, `password`, `first_name`, `last_name`) "
-                                "VALUES ('?', '?', '?', '?')",
+                                "VALUES (?, ?, ?, ?)",
                                 params=[request.form.get("email"),
-                                        hashlib.md5(request.form.get("password")).hexdigest(),
-                                        request.form.get("first_name"), request.form.get("last_name")])
-                return 'ok'
+                                        md5password,
+                                        request.form.get("first_name"), request.form.get("last_name")], commit=True)
+                resp = app.make_response(render_template("process.html", title="Авторизация...",
+                                                         redirect="/account"))
+                resp.set_cookie('id', f'{cur.lastrowid}', max_age=2592000)
+                resp.set_cookie('email', f'{request.form.get("email")}', max_age=2592000)
+                resp.set_cookie('password', f'{md5password}', max_age=2592000)
+                return resp
             else:
-                return error
+                return render_page('register.html', title="Регистрация", error=error)
         else:
-            return "Пользователь с таким адресом электронной почты уже зарегистрирован."
+            return render_page('register.html', title="Регистрация",
+                               error="Пользователь с таким адресом электронной почты уже зарегистрирован.")
     else:
         return render_page('register.html', title="Регистрация")
 
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if isloggin():
+        return redirect('/account')
+    if request.method == 'POST':
+        md5password = request.form.get("password")
+        md5password = md5password.encode(encoding='UTF-8', errors='strict')
+        md5password = str(hashlib.md5(md5password).hexdigest())
+        testuser = databaserequest("SELECT * FROM accounts WHERE `email` = ? AND `password` = ?",
+                                   params=[request.form.get("email"), md5password])
+        if len(testuser) == 0:
+            return render_page('login.html', title="Авторизация", error="Неверный логин или пароль!")
+        else:
+            resp = app.make_response(render_template("process.html", title="Авторизация...",
+                                                     redirect="/account"))
+            resp.set_cookie('id', f'{testuser[0][0]}', max_age=2592000)
+            resp.set_cookie('email', f'{testuser[0][2]}', max_age=2592000)
+            resp.set_cookie('password', f'{testuser[0][1]}', max_age=2592000)
+            return resp
+    else:
+        return render_page('login.html', title="Авторизация")
+
+
+@app.route('/account', methods=['GET', 'POST'])
+def account():
+    acc_info = databaserequest("SELECT * FROM accounts WHERE `id`=?", params=request.cookies.get("id"))[0]
+    if request.method == "POST":
+        if request.form.get("action") == "setrole":
+            if request.form.get("role") == "expert":
+                databaserequest("UPDATE accounts SET `acctype`=2 WHERE `id`=?",
+                                params=[request.cookies.get("id")], commit=True)
+                role = "Эксперт"
+                role_num = 2
+            else:
+                databaserequest("UPDATE accounts SET `acctype`=1 WHERE `id`=?",
+                                params=[request.cookies.get("id")], commit=True)
+                role = "Клиент"
+                role_num = 1
+            return render_page("account.html", title="Аккаунт", loggin=True, acc_id=request.cookies.get("id"),
+                           acc_email=request.cookies.get("email"), acc_password=request.cookies.get("password"),
+                               notification=f"Ваша роль установлена на {role}!", notification_type="green",
+                               acc_type=role_num)
+    else:
+        return render_page("account.html", title="Аккаунт", loggin=True, acc_id=request.cookies.get("id"),
+                           acc_email=request.cookies.get("email"), acc_password=request.cookies.get("password"),
+                           acc_type=acc_info[5])
+
+
+@app.route("/logout")
+def logout():
+    resp = app.make_response(render_template("process.html",
+                                             title="Выход из аккаунта...",
+                                             redirect="/login"))
+    resp.set_cookie('acc_id', f'', max_age=2592000)
+    resp.set_cookie('email', f'', max_age=2592000)
+    resp.set_cookie('password', f'', max_age=2592000)
+    return resp
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_page('error.html', title="Упс...", error="Страница не найдена!")
+    return render_page('error.html', title="Упс...", error="Страница не найдена!"), 404
 
 
 @app.errorhandler(500)
 def page_not_found(e):
     print("ОШИБКА:\n")
     pprint(e)
-    return render_template('error.html', title="Ой... Ошибка...", errors="Произошла ошибка при обработке Вашего запроса!")
+    return render_template('error.html', title="Ой... Ошибка...",
+                           error="Произошла ошибка при обработке Вашего запроса!"), 500
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
     con.close()
